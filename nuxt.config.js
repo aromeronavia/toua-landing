a0c1a81a834b4d3914da22c51b6b7d080d4771a1
Alberto Romero <lguitarras0594@gmail.com>
2021-02-16 00:40:16 -0600

    Add tailwind and build info

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -42,5 +43,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  buildDir: 'dist',
 }
